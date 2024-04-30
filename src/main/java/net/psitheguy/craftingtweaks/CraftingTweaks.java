548bf46365d1eba2def54726dfc0649b61f87128
PsiTheGuy <bobdoor.nm@gmail.com>
2024-04-30 18:31:58 -0400

    1.0

---
+++ src/main/java/net/psitheguy/craftingtweaks/CraftingTweaks.java
@@ -0,0 +1,16 @@
+package net.psitheguy.craftingtweaks;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CraftingTweaks implements ModInitializer {
+	public static final String MOD_ID = "psis-crafting-tweaks";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/psitheguy/craftingtweaks/CraftingTweaksClient.java
@@ -0,0 +1,10 @@
+package net.psitheguy.craftingtweaks;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CraftingTweaksClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
